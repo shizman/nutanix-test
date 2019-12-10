94bc451ea60f6630380338b95a2c5dc126e438b6
shizman <shizman@users.noreply.github.com>
2019-12-10 12:41:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,12 +59,12 @@ locals {
 #   vm_id = nutanix_virtual_machine.vm1.id
 # }
 ### Image Data Sources
-#data "nutanix_image" "Windows_10_Disk" {
-#     metadata = {
-#         kind = "image"
-#     }
-#     image_id = nutanix_image.Windows_10_Disk.id
-# }
+data "nutanix_image" "Windows_10_Disk" {
+     metadata = {
+         kind = "image"
+     }
+     image_id = nutanix_image.Windows_10_Disk.id
+ }
 ### Subnet Data Sources
 # data "nutanix_subnet" "next-iac-managed" {
 #     metadata = {
@@ -99,13 +99,13 @@ locals {
 #   presented here as separate resources, or existing images on a cluster can be
 #   called in as data sources, which you can see in the data sources section
 #   above.
-resource "nutanix_image" "Windows_10_Disk" {
-  name = "Windows_10_Disk"
+# resource "nutanix_image" "Windows_10_Disk" {
+#  name = "Windows_10_Disk"
 
   #source_uri  = "http://endor.dyn.nutanix.com/acro_images/DISKs/cirros-0.3.4-x86_64-disk.img"
   #source_uri  = "http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img"
 #  description = "Windows 10 Disk"
-}
+# }
 
 ### Subnet Resources (Virtual Networks within AHV)
 ## Related Product Docs:
